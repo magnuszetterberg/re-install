ed297bd6be9e9669a591d951df34905eb566ebe2
Magnus Zetterberg <Magnus.Zetterberg@gmail.com>
2023-04-26 09:41:25 +0200

    adding checks if files already exists or not

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
-
-# refresh system first
-echo "** Updateing apt and upgrading**"
+# Refresh system first
+echo "** Update apt and upgrade**"
 echo ""
 echo ""
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove
 echo ""
 echo ""
 sleep 2
-#install usefull tools!
+# Install usefull tools!
 echo "** Installing tools through apt **"
 echo ""
 echo ""
@@ -22,26 +20,35 @@ echo ""
 echo ""
 sleep 2
 
-# create new ssh-key
+# Create new ssh-key
 echo "** creating ssh-key **"
 echo ""
 echo ""
-#ssh-keygen
+# check here if a file already exist - if true - skip
+if [ -f ~/.ssh/id_rsa.pub ]; then
+    echo "id_rsa.pub file already exist, skipping"
+    echo ""
+    echo ""
+else
+    echo ""
+    echo ""
+    echo "ssh-keygen"
+fi
 sleep 2
 
-# refresh snap
+# Refresh snap
 echo "** refreshing snap store **"
 echo ""
 echo ""
 sudo snap refresh
 sleep 2
-# install snaps
+# Install snaps
 echo "** installing snap tools **"
 echo ""
 echo ""
 sudo snap install \
  chromium \
- code \
+ code
 sleep 2
 
 #Setup zero-tier network
@@ -49,5 +56,37 @@ echo "** installing zero-tier client **"
 echo ""
 echo ""
 curl -s https://install.zerotier.com | sudo bash
-
 sleep 2
+
+echo ""
+echo ""
+# Setup starship shell
+echo "** Installing Starship shell **"
+echo ""
+echo ""
+curl -sS https://starship.rs/install.sh | sh
+sleep 2
+
+# Adding setup config to ~/.bashrc for starship
+echo "** Adding starship config to ~/.bashrc file **"
+# check here if a file already exist - if true - skip
+if grep -q "export STARSHIP_CONFIG*" ~/.bashrc; then
+    echo "starship config already exists in ~/.bashrc, skipping"
+    echo ""
+    echo ""
+else
+    echo "cat .bashrc-starship >> ~/.bashrc"
+fi
+sleep 2
+
+# Copying starship.toml file to ~/.config/
+echo "** Copying starship.toml file to ~/.config folder"
+# check here if a file already exist - if true - skip
+if [ -f ~/.config/starship.toml ]; then
+    echo "starship.toml file already exist, skipping"
+    echo ""
+    echo ""
+else
+    mkdir -p ~/.config && cp starship.toml ~/.config/starship.toml
+fi
+
